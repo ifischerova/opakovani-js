daa9fddcca7c74f42fd8774a1941f6c1fb05a015
Ivuska <fischerova.ivka@gmail.com>
2023-02-06 14:21:43 +0100

    Solution of JS repetition.

--- index.js
+++ index.js
@@ -1,4 +1,56 @@
 'use strict';
 
 /* tady bude tvůj kód */
-console.log('JavaScript ve stránce funguje');
+let movies = []
+fetch('https://apps.kodim.cz/daweb/trening-api/apis/movie-api/movies')
+  .then((response) => response.json())
+  .then((data) => showMovies(data)); 
+
+// Parametry musi mit stejny nazev, jako jsou jednotlive klice v jsonu
+
+// Pred destrukturalizaci
+/* const movieItem = (movie) => {
+  return `
+      <div class="movie">
+      <img class="movie__img" src="${movie.posterUrl}" alt="${movie.title}">
+          <a href="${movie.url}"><h2 class="movie__title">${movie.title}</h2></a>
+          <p class="movie__year">${movie.year}</p>
+          <p class="movie__genre">${movie.genres}</p>
+      </div>
+  `;
+}*/
+
+//Destrukturalizovany objekt
+const Movie = ({ title, posterUrl, url, year, genres }) => {
+  return `
+      <div class="movie">
+      <img class="movie__img" src="${posterUrl}" alt="${title}">
+          <a href="${url}"><h2 class="movie__title">${title}</h2></a>
+          <p class="movie__year">${year}</p>
+          <p class="movie__genre">${genres}</p>
+      </div>
+  `;
+};
+
+// div s id "movies" do ktereho vypisuji jednotlive filmy
+const movieList = document.querySelector('#movies')
+
+const showMovies = (movies, asc = true) => {
+  //porovnava nazvy filmu, vzdy dva proti sobe
+  //localeCompare je kvuli diakritice
+  movies.sort((movieA, movieB) => movieA.title.localeCompare(movieB.title, 'cs'));
+  if (asc === false ) {
+    movies.reverse();
+  }
+
+// z kazdeho filmu udela komponentu a tu pak nahraje do HTML, nahrava jednu komponentu po druhe.
+  movies.forEach((movie) => {
+    movieList.innerHTML += Movie(movie)
+  })
+
+  // Vytvori seznam komponent a ty pak najednou nahraje do HTML
+  //movieList.innerHTML = movies.map((movie) => Movie(movie)).join('');
+}
+
+document.querySelector('#button-asc').addEventListener("click", () => showMovies(movies, true));
+document.querySelector('#button-desc').addEventListener("click", () => showMovies(movies, false));
